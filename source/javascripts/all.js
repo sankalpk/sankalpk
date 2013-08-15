04d0e95800bd350d017297a6303c4125fdc7dcd3
Sankalp Kulshreshta <sankalp221@gmail.com>
2013-08-14 21:19:00 -0400

    Initial commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,34 @@
+//= require_tree .
+//= require "jquery"
+
+$(document).ready(function(){
+
+	$("#letsconnect").click(connectHandler);
+});
+
+
+connectHandler = function(){
+	var connect = $("#connect");
+	if(connect.css("right")==="-300px") openConnect();
+	else closeConnect();
+}
+
+openConnect = function(){
+	var connect = $("#connect");
+	if(connect.css("right")==="-300px")
+		connect.animate({right: "+=300"},250,easeInOutQuad);
+	else connect.css("right","0px");
+}
+
+closeConnect = function(){
+	var connect = $("#connect");
+	if(connect.css("right")==="0px")
+		connect.animate({right: "-=300"},250,easeInOutQuad);
+	else
+		connect.css("right",-300);
+}
+
+easeInOutQuad = function (x, t, b, c, d) {
+        if ((t/=d/2) < 1) return c/2*t*t + b;
+        return -c/2 * ((--t)*(t-2) - 1) + b;
+}
